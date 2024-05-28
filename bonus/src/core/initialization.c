9ddd72e773665ee64b70118255b2482acd1b90bb
ebmarque <ebenezermarquezine@gmail.com>
2024-05-29 00:47:57 +0100

    norminette errors corrected in initialization.c in bonus

--- bonus/src/core/initialization.c
+++ bonus/src/core/initialization.c
@@ -6,32 +6,19 @@
 /*   By: ebmarque <ebmarque@student.42porto.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 16:17:13 by ebmarque          #+#    #+#             */
-/*   Updated: 2024/05/29 00:26:42 by ebmarque         ###   ########.fr       */
+/*   Updated: 2024/05/29 00:46:55 by ebmarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/core.h"
 
-void	_load_sprite()
-{
-	cubed()->sprite = ft_calloc(1, sizeof(t_sprite));
-	cubed()->sprite->sprites = ft_calloc(NB_SPRITES, sizeof(t_texture));
-	cubed()->sprite->sprites[0] = _upload_texture(SPRITE_1);
-	cubed()->sprite->sprites[1] = _upload_texture(SPRITE_2);
-	cubed()->sprite->sprites[2] = _upload_texture(SPRITE_3);
-	cubed()->sprite->current_frame = 0;
-	cubed()->sprite->time = clock();
-}
-
 void	_load_textures(void)
 {
-	cubed()->texture = ft_calloc(5 ,sizeof(t_texture));
+	cubed()->texture = ft_calloc(5, sizeof(t_texture));
 	cubed()->texture[NORTH] = _upload_texture(cubed()->content->no_t);
 	cubed()->texture[SOUTH] = _upload_texture(cubed()->content->so_t);
 	cubed()->texture[EAST] = _upload_texture(cubed()->content->ea_t);
 	cubed()->texture[WEST] = _upload_texture(cubed()->content->we_t);
-	cubed()->door = _upload_texture(DOOR_PATH);
-	_load_sprite();
 }
 
 void	_init_gmap(void)
@@ -40,11 +27,13 @@ void	_init_gmap(void)
 
 	mini = ft_calloc(1, sizeof(t_gmap));
 	mini->map = cubed()->content->map;
-	mini->tile = (float)((WIDTH * 0.25) / cubed()->content->matrix_dimensions.x);
+	mini->tile = \
+		(float)((WIDTH * 0.25) / cubed()->content->matrix_dimensions.x);
 	mini->player.x = cubed()->player->pos.x * mini->tile;
 	mini->player.y = cubed()->player->pos.y * mini->tile;
 	mini->player.is_set = 1;
 	mini->player.teta = 0;
+	_init_img(&mini->map_img);
 	cubed()->gmap = mini;
 	cubed()->player->map_view = 1;
 }
@@ -52,8 +41,8 @@ void	_init_gmap(void)
 t_texture	*_upload_texture(char *path)
 {
 	t_texture	*text;
-	
-	text = ft_calloc(1 ,sizeof(t_texture));
+
+	text = ft_calloc(1, sizeof(t_texture));
 	text->img = mlx_xpm_file_to_image(cubed()->mx_var->mlx, path,
 			&(text->width), &(text->height));
 	text->addr = mlx_get_data_addr(text->img, &text->bpp, &text->line_len,
@@ -63,12 +52,16 @@ t_texture	*_upload_texture(char *path)
 
 void	update_player(void)
 {
-	if (cubed()->player->pos.teta == PI/2)
-		cubed()->player->plane = (t_pos){+0.66,0,0,0};
-	else if (cubed()->player->pos.teta == 3 * (PI/2))
-		cubed()->player->plane = (t_pos){-0.66,0,0,0};
+	t_pos	p;
+
+	p = cubed()->player->plane;
+	if (cubed()->player->pos.teta == PI / 2)
+		p = (t_pos){+0.66, 0, 0, 0};
+	else if (cubed()->player->pos.teta == 3 * (PI / 2))
+		p = (t_pos){-0.66, 0, 0, 0};
 	else if (cubed()->player->pos.teta == PI)
-		cubed()->player->plane = (t_pos){0,+0.66,0,0};
+		p = (t_pos){0, +0.66, 0, 0};
 	else if (cubed()->player->pos.teta == 0)
-		cubed()->player->plane = (t_pos){0,-0.66,0,0};
+		p = (t_pos){0, -0.66, 0, 0};
+	cubed()->player->plane = p;
 }
